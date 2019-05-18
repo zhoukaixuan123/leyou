d95308457192f61e6d4b834ee3558a797cf1bd77
zhoukaixuan123 <15030610683@163.com>
2019-05-18 20:26:51 +0800

    页面静态化处理

--- ly-page/src/main/java/com/leyou/page/service/PageService.java
+++ ly-page/src/main/java/com/leyou/page/service/PageService.java
@@ -5,9 +5,14 @@ import com.leyou.page.client.BrandClient;
 import com.leyou.page.client.CategoryClient;
 import com.leyou.page.client.GoodsClient;
 import com.leyou.page.client.SpecificationClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
+import java.io.File;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -19,6 +24,7 @@ import java.util.Map;
  * @date 2019/5/18$
  * @description $
  */
+@Slf4j
 @Service
 public class PageService {
 
@@ -30,6 +36,11 @@ public class PageService {
     private GoodsClient goodsClient;
     @Autowired
     private SpecificationClient specificationClient;
+    //页面静态化实现类
+    @Autowired
+    private TemplateEngine engine;
+
+
 
     public Map<String, Object> loadMpdel(Long spuId) {
         Map<String, Object> map = new HashMap<>();
@@ -46,8 +57,9 @@ public class PageService {
         //查询规格参数
         Long cid3 = spu.getCid3();
         List<Specification> specs = specificationClient.querySpecsByCid(cid3);
-        map.put("title", spu.getTitle());
-        map.put("subTitle", spu.getSubTitle());
+        // map.put("title", spu.getTitle());
+        // map.put("subTitle", spu.getSubTitle());
+        map.put("spu", spu);
         map.put("skus", skus);
         map.put("detail", detail);
         map.put("brand", brand);
@@ -55,4 +67,32 @@ public class PageService {
         map.put("specs", specs);
         return map;
     }
+
+    //页面静态化
+    public void createHtml(Long spuId) {
+        //初始化输出流
+        PrintWriter writer = null;
+        try {
+            //初始化上下  文
+            Context context = new Context();
+            //初始化数据
+            Map<String, Object> map = this.loadMpdel(spuId);
+            //把数据放到context 中
+            context.setVariables(map);
+            //写入文件
+            File file = new File("F:\\upload\\page",spuId+".html");
+            writer = new PrintWriter(file,"UTF-8");
+            engine.process("item", context, writer);
+        } catch (Exception ex) {
+
+            log.error(   "创建模板出错：" +spuId);
+        } finally {
+            if(writer != null ){
+                writer.close();
+            }
+
+        }
+
+    }
+
 }
